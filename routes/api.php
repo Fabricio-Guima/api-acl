7643e1c2afcd4e30df6b67b665b97d08de2d7a12
Fabricio_Guima <fsgkof@gmail.com>
2024-07-29 13:16:53 -0300

    create user route

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
-
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
 
 
 
