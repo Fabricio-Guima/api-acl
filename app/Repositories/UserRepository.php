e01a0c68bb7838887009e621af449865f9686e26
Fabricio_Guima <fsgkof@gmail.com>
2024-07-29 12:21:00 -0300

    user repository

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,10 +3,19 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class UserRepository
 {
     public function __construct(protected User $user)
     {
+    }
+
+    public function getAllUsers(string $filter = ''): LengthAwarePaginator
+    {
+        return $this->user->when(!empty($filter), function ($query) use ($filter) {
+            $query->where('name', 'LIKE', "%{$filter}%");
+        })->paginate();
     }
 }
