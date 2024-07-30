0ff352d387e16fe9dd1e145b987ec37dd324c904
Fabricio_Guima <fsgkof@gmail.com>
2024-07-30 17:05:17 -0300

    update user route

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
-
+Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
 
 
 Route::get('/', fn () => response()->json(['message' => 'Hello World!']));
