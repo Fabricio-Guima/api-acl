65408bce28548dbe2fcaf4fcf18a614894a5ebab
Fabricio_Guima <fsgkof@gmail.com>
2024-07-30 17:18:43 -0300

    user delete route

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show')
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-
+Route::delete('/users/{user}',[UserController::class, 'destroy'])->name('users.destroy');
 
 Route::get('/', fn () => response()->json(['message' => 'Hello World!']));
