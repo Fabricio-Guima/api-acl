c35f34e11f606e81ee16e9446a64efc09dcd7a01
Fabricio_Guima <fsgkof@gmail.com>
2024-08-12 15:02:31 -0300

    api routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,27 @@
 <?php
 
+use App\Http\Controllers\api\Auth\AuthController;
+use App\Http\Controllers\Api\PermissionController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth:sanctum')->post('/me', [AuthController::class, 'me'])->name('auth.me');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}',[UserController::class, 'destroy'])->name('users.destroy');
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::apiResource('/permissions', PermissionController::class);
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}',[UserController::class, 'destroy'])->name('users.destroy');
+});
+
 
 Route::get('/', fn () => response()->json(['message' => 'Hello World!']));
