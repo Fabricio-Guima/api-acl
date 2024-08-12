4bb14e70372e7e05522e745b6040cabbcd3060e2
Fabricio_Guima <fsgkof@gmail.com>
2024-08-12 15:01:24 -0300

    permission model

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -10,6 +10,11 @@ class Permission extends Model
 {
     use HasFactory, HasUuids;
 
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
     public function users()
     {
         return $this->belongsToMany(User::class);}
